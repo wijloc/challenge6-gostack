26448971ffb0fbc8cefb94a64e92c3ab437b044f
Willian Oliveira <wijloc@gmail.com>
2020-11-23 15:34:31 -0300

    feat: create transaction service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,45 @@
+import { getCustomRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+import FindOrInsertCategoryService from './FindOrInsertCategoryService';
+
+interface Request {
+  title: string;
+
+  type: 'income' | 'outcome';
+
+  value: number;
+
+  category_title: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_title,
+  }: Request): Promise<Transaction> {
+    const findOrInsertCategory = new FindOrInsertCategoryService();
+
+    const category = await findOrInsertCategory.execute({
+      title: category_title,
+    });
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
